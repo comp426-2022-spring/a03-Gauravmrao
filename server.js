a183467ae21e4a9a6ba144bb65dcd5fda671934c
Gaurav Rao <69742533+Gauravmrao@users.noreply.github.com>
2022-04-14 16:46:55 -0400

    added the coinflip

--- server.js
+++ server.js
@@ -8,3 +8,40 @@ var port = portNum.port || 5000;
 const server = app.listen(port, () => {
     console.log('App is running on port %PORT%'.replace('%PORT%', port))
 })
+
+
+
+function coinFlip() {
+    let coinValue;
+    let coinNum = Math.random();
+    if (coinNum < 0.5 ) {
+      coinValue = "heads";
+    } else {
+      coinValue = "tails";
+    }
+    return coinValue;
+  }
+
+
+
+app.get('/app', (req, res) => {
+    res.status(200).end('The API is working')
+    res.type('text/plain')
+})
+
+
+app.get('/app/flip', (req, res) => {
+    res.status(200).json({ 'flip' : coinFlip()})
+})
+
+
+/*
+app.get('/app/echo/:number', (req, res) => {
+    res.status(200).json({ 'message': req.params.number })
+})
+*/
+
+app.use(function(req, res){
+    res.status(404).send("Endpoint does not exist")
+    res.type("text/plain")
+})
